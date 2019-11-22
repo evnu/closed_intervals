efd1481885cfaabcee500fe99c767348b4fef81d
Magnus Ottenklinger <evnu@posteo.de>
2019-11-22 23:28:02 +0100

    Initial commit

---
+++ lib/interval_tree.ex
@@ -0,0 +1,18 @@
+defmodule IntervalTree do
+  @moduledoc """
+  Documentation for IntervalTree.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IntervalTree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/interval_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule IntervalTreeTest do
+  use ExUnit.Case
+  doctest IntervalTree
+
+  test "greets the world" do
+    assert IntervalTree.hello() == :world
+  end
+end
