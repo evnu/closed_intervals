1ada22fc7764a572de776e4a6690dd846f136c51
Magnus Ottenklinger <evnu@posteo.de>
2019-11-25 23:18:20 +0100

    mix: Allow older Elixir versions as well

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ClosedIntervals.MixProject do
     [
       app: :closed_intervals,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
